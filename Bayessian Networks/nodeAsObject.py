42f40566c7b4d43b2f1b2bff3fb6d1f59a152e3b
febrianimanda <febrian.imanda@gmail.com>
2016-10-01 17:38:41 +0700

    adding DAG function for addCPT and getCPT

--- Bayessian Networks/nodeAsObject.py
+++ Bayessian Networks/nodeAsObject.py
@@ -32,18 +32,48 @@ example_dag = {
 	}
 }
 
+example_CPT = [
+
+	{
+		'node'	: 'A',
+		'given'	: None,
+		'value'	: [.6, .4]
+	},
+	{
+		'node'	: 'B',
+		'given'	: 'A',
+		'value'	: [.2, .8, .75, .25]
+	},
+	{
+		'node'	: 'E',
+		'given'	: 'C',
+		'value'	: [.7, .3, 0, 1]
+	},
+	{
+		'node'	: 'C',
+		'given'	: 'A',
+		'value'	: [.8, .2, .1, .9]
+	},
+	{
+		'node'	: 'D',
+		'given'	: '[B,C]',
+		'value'	: [.95, .05, .9, .1, .8, .2, 0, 1]
+	}
+]
+
 class DAG:
 	'Common base class for Directed Acyclid Graph'
 
 	def __init__(self, listNode = []):
 		self._nodeCount = 0
 		self._listNode = listNode[:] if len(listNode) == 0 else listNode
+		self._listCPT = []
 
 	def showNodeCounter(self):
 		print 'Total Nodes: %d' % self._nodeCount
 
 	def showAllNode(self):
-		print "Node di dalam DAG : %s" % [x.getAlias() for x in self._listNode]
+		print "Nodes in DAG : %s" % [x.getAlias() for x in self._listNode]
 
 	def addNode(self, node):
 		if isinstance(node, list):
@@ -53,6 +83,42 @@ class DAG:
 		else:
 				self._nodeCount += 1
 				self._listNode.append(node)
+
+	# with weaknes CPT name is not flexible
+	# def addCPT(self, node, givenNode, listValue):
+	# 	islist = isinstance(givenNode, list)
+	# 	length = 1 + (len(givenNode) if (islist == True) else 1)
+	# 	name = "%s|%s" % (node.getAlias(), ''.join([x.getAlias() for x in givenNode]) if islist else givenNode.getAlias())
+	# 	if 2**length == len(listValue):
+	# 		self._listCPT[name] = listValue
+	# 		print "CPT successfully added"
+	# 	else:
+	# 		print "CPT seems not properly, please try again"
+
+	def addCPT(self, node, givenNode, listValue):
+		islist = isinstance(givenNode, list)
+		length = 1 + (len(givenNode) if (islist == True) else 1)
+		iCPT = len(self._listCPT) + 1
+		if 2**length == len(listValue):
+			obj = {
+				'name' : '%s given %s' % (node.getAlias(), ''.join([x.getAlias() for x in givenNode]) if islist else givenNode.getAlias()),
+				'node' : node,
+				'given' : givenNode,
+				'value' : listValue,
+			}
+			self._listCPT.append(obj)
+			print "CPT %s given %s successfully added" % (node.getAlias(), ''.join([x.getAlias() for x in givenNode]) if islist else givenNode.getAlias())
+		else:
+			print "CPT seems not properly, please try again"
+
+	def getCPT(self, alias, given):
+		return [x for x in self._listCPT if x['node'] == alias and x['given'] == given]
+
+	def getNode(self, alias):
+		for x in self._listNode:
+			if x.getAlias() == alias :
+				return x
+		return None
 
 class Node:
 	'Common base class for Node in DAG'
@@ -115,7 +181,6 @@ class Node:
 	def showParent(self):
 		print [node.getAlias() for node in self._parent]
 
-
 dag = DAG()
 A = Node('A', 'Winter')
 B = Node('B', 'Sprinkler', [A])
@@ -129,4 +194,7 @@ C.addConnection([D])
 
 dag.addNode([A,B,C,D,E])
 dag.showAllNode()
-
+dag.addCPT(B,A,[.2, .8, .75, .25])
+dag.addCPT(E,C,[.7, .3, 0, 1])
+dag.addCPT(C,A,[.8, .2, .1, .9])
+dag.addCPT(D,[B,C],[.95, .05, .9, .1, .8, .2, 0, 1])
