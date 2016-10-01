e80bcfe0c3e274e9b88a8ac43b6804c6b2f0106d
febrianimanda <febrian.imanda@gmail.com>
2016-10-02 00:29:07 +0700

    modifying addCPT and isTrail function

--- Bayessian Networks/nodeAsObject.py
+++ Bayessian Networks/nodeAsObject.py
@@ -85,10 +85,9 @@ class DAG:
 		self._listCPT = []
 
 	def showNodeCounter(self):
-		print 'Total Nodes: %d' % self._nodeCount
+		print 'Total Nodes in DAG: %d' % self._nodeCount
 
 	def showAllNode(self):
-		# print "Nodes in DAG : %s" % [x['node'].getAlias() for x in self._listNode]
 		for i in self._listNode:
 			print i
 
@@ -103,17 +102,6 @@ class DAG:
 				self._nodeCount += 1
 				self._listNode.append(obj)
 
-	# with weaknes CPT name is not flexible
-	# def addCPT(self, node, givenNode, listValue):
-	# 	islist = isinstance(givenNode, list)
-	# 	length = 1 + (len(givenNode) if (islist == True) else 1)
-	# 	name = "%s|%s" % (node.getAlias(), ''.join([x.getAlias() for x in givenNode]) if islist else givenNode.getAlias())
-	# 	if 2**length == len(listValue):
-	# 		self._listCPT[name] = listValue
-	# 		print "CPT successfully added"
-	# 	else:
-	# 		print "CPT seems not properly, please try again"
-
 	def addConnection(self, node, connection, status='child'):
 		islist = isinstance(connection, list)
 		if status == 'parent' :
@@ -126,7 +114,7 @@ class DAG:
 				self.getNode(node)['child'].extend(connection)
 			else:
 				self.getNode(node)['child'].append(connection)
-		print "Node %s: Node %s sebagai %s berhasil dihubungkan " % (node, connection, status)
+		print "Node %s: Node %s as %s successfully connected " % (node, connection, status)
 
 	def addCPT(self, node, givenNode, listValue):
 		islist = isinstance(givenNode, list)
@@ -134,32 +122,33 @@ class DAG:
 		iCPT = len(self._listCPT) + 1
 		if 2**length == len(listValue):
 			obj = {
-				'name' : '%s given %s' % (node.getAlias(), ''.join([x.getAlias() for x in givenNode]) if islist else givenNode.getAlias()),
-				'node' : node,
-				'given' : givenNode,
-				'value' : listValue,
+				'name' : '%s|%s' % (node, ''.join([x.getAlias() for x in givenNode]) if islist else givenNode.getAlias()),
+				'node' : node, 'given' : givenNode, 'value' : listValue,
 			}
 			self._listCPT.append(obj)
-			print "CPT %s given %s successfully added" % (node.getAlias(), ''.join([x.getAlias() for x in givenNode]) if islist else givenNode.getAlias())
+			print "CPT %s|%s successfully added" % (node, ''.join([x.getAlias() for x in givenNode]) if islist else givenNode.getAlias())
 		else:
 			print "CPT seems not properly, please try again"
 
-	def getCPT(self, alias, given):
-		return [x for x in self._listCPT if x['node'] == alias and x['given'] == given]
+	def getAllCPT(self):
+		return self._listCPT
+
+	def getCPT(self, node, given):
+		return [x for x in self._listCPT if x['node'] == node and x['given'] == given]
 
 	def isTrail(self, nodeA, nodeB, visited = []):
 		visited = visited[:] if len(visited) == 0 else visited
 		if nodeA not in visited:
 			visited.append(nodeA)
-			if nodeA.isHaveChild():
-				for x in nodeA.getChild():
+			if self.isNodeHave(nodeA, 'child'):
+				for x in self.getNode(nodeA)['child']:
 					if x == nodeB :
 						return True
 					else:
 						if self.isTrail(x, nodeB, visited):
 							return True
-			elif nodeA.isHaveParent():
-				for x in nodeA.getParent():
+			elif self.isNodeHave(nodeA, 'parent'):
+				for x in self.getNode(nodeA)['parent']:
 					if x == nodeB:
 						return True
 					else:
@@ -185,15 +174,16 @@ class DAG:
 			return len(obj[things]) != 0
 		return False
 
-	def isHaveParent(self):
-		return len(self._parent) != 0
-
 	def showFromNode(self, node, things):
 		obj = self.getNode(node)
 		if obj != None:
 			print 'Child Node %s : %s' % (node, obj[things])
 		else:
 			print 'Node tidak ditemukan'
+
+	def showAllCPT(self):
+		for x in self._listCPT:
+			print x
 
 class Node:
 	'Common base class for Node in DAG'
@@ -237,8 +227,9 @@ dag.addConnection(c,e)
 dag.addConnection(e,c,'parent')
 dag.showAllNode()
 dag.showFromNode(a, 'child')
-
-# dag.addCPT(B,A,[.2, .8, .75, .25])
-# dag.addCPT(E,C,[.7, .3, 0, 1])
-# dag.addCPT(C,A,[.8, .2, .1, .9])
-# dag.addCPT(D,[B,C],[.95, .05, .9, .1, .8, .2, 0, 1])
+dag.addCPT(b,a,[.2, .8, .75, .25])
+dag.addCPT(e,c,[.7, .3, 0, 1])
+dag.addCPT(c,a,[.8, .2, .1, .9])
+dag.addCPT(d,[b,c],[.95, .05, .9, .1, .8, .2, 0, 1])
+dag.showAllCPT()
+print dag.isTrail(d,e)
